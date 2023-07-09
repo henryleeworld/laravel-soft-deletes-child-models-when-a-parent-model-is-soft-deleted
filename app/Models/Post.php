0b939bb1a8bf1a844a05b2103eab785d5e5cffeb
Henry Lee <henryleeworld@gmail.com>
2023-07-10 00:17:46 +0800

    feat:升級至 laravel 9

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,8 @@ namespace App\Models;
 
 use Dillingham\SoftDeletesParent\SoftDeletesParent;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use SoftDeletes, SoftDeletesParent;
-
-    protected static $softDeletesParent = Author::class;
+    use SoftDeletesParent;
 }
